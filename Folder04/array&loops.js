0f579cd3dc09622a8bcaede09955b47d7a65fed8
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-07-10 20:02:47 +0530

    added

--- Folder04/array&loops.js
+++ Folder04/array&loops.js
@@ -2,9 +2,29 @@ let arr = [];
 
 function addTodo(){
 
-let texted = document.getElementById('text2');
+let texted = document.getElementById('text1');
 
 let extractedValue = texted.value;
 arr.push(extractedValue)
 console.log(arr);
+
+texted.value = '';
+}
+
+let arr2 = []
+
+function addTodo2(){
+  let text = document.getElementById('text2');
+  let addList = text.value;
+
+  arr2.push(addList);
+
+  // let displayResult = document.getElementById('displayText').innerHTML = `${arr2.push(textValue.value)}`;
+  // console.log(arr2);
+  // console.log(displayResult)
+  console.log(arr2)
+
+  text.value = '';
+
+
 }
